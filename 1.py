d9f4c6e295350daf6e3a71bb861afd10d4319681
hurttttr <945399100@qq.com>
2021-06-10 21:35:32 +0800

    2021年6月10日

--- 1.py
+++ 1.py
@@ -1,4 +1,20 @@
-lst = [25, 11, 26, 7, 15, 20, 4, 12, 17, 19, 16, 10, 6, 3,
-       23, 1, 18, 28, 24, 0, 22, 13, 14, 5, 21, 29, 8, 27, 9, 2]
-lst.sort()
+import requests
+import re
+from lxml import etree
+
+pat = '<a href="(.*?)" title'
+url = 'https://zh.m.wikisource.org/zh/%E5%94%90%E8%A9%A9%E4%B8%89%E7%99%BE%E9%A6%96'
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36',
+}
+
+
+response = requests.get(url, headers=headers)
+html = response.content.decode()
+element = etree.HTML(html)
+lst = element.xpath('//*[@id="mw-content-text"]/div/section')
+# lst = re.compile(pat, re.S).findall(html)
 print(lst)
+for i in lst[2:]:
+    t = i.xpath('./ol/li/a/@href')
+    print(t)
