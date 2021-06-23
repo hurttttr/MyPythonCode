5bf0dd21773e3404fa200fb4ea9664ac1e72c55f
hu_qi <58456022+hurttttr@users.noreply.github.com>
2021-06-23 15:27:06 +0800

    添加丢失的代码

---
+++ 网络编程/1822-qqServer.py
@@ -0,0 +1,63 @@
+# 案列使用TCP连接
+# 这是服务器端
+
+import socket
+import wx
+#  定义套接字 s
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+# 定义窗口类
+class MyFrame(wx.Frame):
+    def __init__(self):
+        super().__init__(parent=None,size=(350,250),title="QQ聊天：服务器")
+        self.Center()
+        panel=wx.Panel(self)
+        self.text = wx.TextCtrl(parent=panel, id=-1, style=wx.TE_MULTILINE,size=(100,150))
+        startbutton = wx.Button(parent=panel, id=1, label="开启服务器")
+        self.inputtext = wx.TextCtrl(parent=panel, id=-1)
+        button=wx.Button(parent=panel,id=2,label="发送")
+        # 绑定按钮事件
+        self.Bind(wx.EVT_BUTTON, self.Talk, id=1)
+        self.Bind(wx.EVT_BUTTON,self.on_button,id=2)
+        box1=wx.BoxSizer()
+        box1.Add(startbutton, 1, flag=wx.ALL | wx.EXPAND, border=10)
+        box1.Add(self.inputtext,1,flag=wx.ALL|wx.EXPAND,border=10)
+        box1.Add(button,1,flag=wx.ALL|wx.EXPAND,border=10)
+        mainbox=wx.BoxSizer(wx.VERTICAL)
+        mainbox.Add(self.text,flag=wx.EXPAND)
+        mainbox.Add(box1, flag=wx.EXPAND)
+        panel.SetSizer(mainbox)
+
+    # 开启服务器端函数
+    def Talk(self,event):
+        s.bind(("127.0.0.1", 8888))
+        s.listen(5)
+        print("服务器启动·····")
+        self.conn,self.addess=s.accept()
+        data =self.conn.recv(1024)
+        if len(data) != 0: #
+            self.text.LabelText += "客户端 对 服务器端 说：" + data.decode() + "\r\n"
+
+    # 发送信息给客户端函数
+    def on_button(self,event):
+        msg=self.inputtext.GetValue()
+        self.conn.send(msg.encode())
+        self.text.LabelText+="服务器端 对 客户端说："+msg+"\r\n"
+
+        data = self.conn.recv(1024)
+        if len(data) != 0:
+            self.text.LabelText +="客户端 对 服务器端 说："+data.decode()+"\r\n"
+
+# 应用程序
+class App(wx.App):
+    def OnInit(self):
+        frame=MyFrame()
+        frame.Show()
+        return True
+    def OnExit(self):
+        s.close() # 关闭socket对象
+        return 0
+
+if __name__=="__main__":
+    app=App()
+    app.MainLoop()
---
+++ 网络编程/2707（cilent）.py
@@ -0,0 +1,60 @@
+import socket
+import wx
+
+#  定义套接字 s
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# 定义窗口类
+class MyFrame(wx.Frame):
+    # 初始化这里就是生成界面，然后绑定了按钮事件，其他没了
+    def __init__(self):
+        super().__init__(parent=None, size=(350, 250), title="圆的面积：客户端")
+        self.Center()
+        panel = wx.Panel(self)
+        self.text = wx.TextCtrl(parent=panel, id=-1,
+                                style=wx.TE_MULTILINE, size=(100, 150))
+        startbutton = wx.Button(parent=panel, id=1, label="连接服务器")
+        self.inputtext = wx.TextCtrl(parent=panel, id=-1)
+        # st = wx.StaticBox(parent=panel, id=-1, label="Enter a radius:")
+        button = wx.Button(parent=panel, id=2, label="发送")
+        self.Bind(wx.EVT_BUTTON, self.Talk, id=1)
+        # self.Bind(wx.EVT_TEXT_ENTER, self.on_button, id=2)
+        self.Bind(wx.EVT_BUTTON, self.on_button, id=2)
+        box1 = wx.BoxSizer()
+        box1.Add(startbutton, 1, flag=wx.ALL | wx.EXPAND, border=10)
+        # box1.Add(st, 1, flag=wx.ALL | wx.EXPAND, border=10)
+        box1.Add(self.inputtext, 1, flag=wx.ALL | wx.EXPAND, border=10)
+        box1.Add(button, 1, flag=wx.ALL | wx.EXPAND, border=10)
+        mainbox = wx.BoxSizer(wx.VERTICAL)
+        mainbox.Add(self.text, flag=wx.EXPAND)
+        mainbox.Add(box1, flag=wx.EXPAND)
+        panel.SetSizer(mainbox)
+
+    # 连接服务器函数
+    def Talk(self, event):
+        s.connect(("127.0.0.1", 8888))
+        print('连接成功')
+        return
+
+    # 发送信息给服务器端
+    def on_button(self, event):
+        msg = self.inputtext.GetValue()
+        s.send(msg.encode())
+        msg = float(msg)
+        self.text.LabelText += "Radius is：{:.1f}\r\n".format(msg)
+
+        data = s.recv(1024)
+        if data:
+            self.text.LabelText += "Area received from the server:{}\r\n".format(float(data.decode()))
+
+
+class App(wx.App):
+    def OnInit(self):
+        frame = MyFrame()
+        frame.Show()
+        return True
+
+
+if __name__ == "__main__":
+    app = App()
+    app.MainLoop()
